688b516a4e5b8ea972e625dea8132d790a069a86
andrealornz <andrea_lorenzo@dlsu.edu.ph>
2025-06-24 20:50:29 +0800

    fixed createCalendar
    adding to public list is redundant as it is already added in Calendar constructor

--- Account.java
+++ Account.java
@@ -80,7 +80,7 @@ public class Account {
         @param newName
         @param newAccess
      */
-public Calendar createCalendar(String newName, boolean newAccess) {
+    public Calendar createCalendar(String newName, boolean newAccess) {
         Calendar newCalendar = null;
         if (!newName.equals(this.username)) { //check if newName doesnt match username
             newCalendar = new Calendar(newName, this.username, newAccess);
